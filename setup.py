186f1b4137d5802864c6ad25a3f3d56d406fbd46
AlvaroCavalcante <leandro0807@live.com>
2026-02-12 10:32:55 -0300

    ajusting project folder structure

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="airflow-calendar",
+    version="0.1.0",
+    description="A sleek, Google Calendar-inspired global view for Apache Airflow DAGs.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Alvaro Carneiro",
+    url="https://github.com/AlvaroCavalcante/airflow-calendar-plugin",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "apache-airflow>=2.0.0",
+    ],
+    entry_points={
+        "airflow.plugins": [
+            "global_schedule = airflow_calendar_plugin:GlobalSchedulePlugin"
+        ],
+    },
+    classifiers=[
+        "Framework :: Apache Airflow",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
