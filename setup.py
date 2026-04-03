1f4e882e99bec576b7ed00f9cbacac60ebacd210
Alvaro Leandro Cavalcante Carneiro <leandro0807@live.com>
2026-04-03 15:19:05 -0300

    updating package metadata

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name="airflow_calendar", 
     version="0.6.1",
-    description="A sleek, Google Calendar-inspired global view for Apache Airflow DAGs.",
+    description="A modern and intuitive calendar interface for visualizing your DAG schedules in Apache Airflow.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Alvaro Carneiro",
+    author_email='alvaroleandro250@gmail.com',
     url="https://github.com/AlvaroCavalcante/airflow-calendar-plugin",
     packages=find_packages(),
+    license='Apache License 2.0',
     include_package_data=True,
     zip_safe=False,
+
     install_requires=[
         "apache-airflow>=2.0.0",
         "croniter==5.0.1",
@@ -22,8 +25,29 @@ setup(
         ],
     },
     classifiers=[
-        "Framework :: Apache Airflow",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: System :: Benchmark',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Framework :: Apache Airflow'
     ],
+    keywords=[
+        'airflow',
+        'python',
+        'python3',
+        'dag',
+        'calendar',
+        'apache',
+        'data-engineering',
+        'schedule',
+        'scheduler',
+        'visualization',
+        'airflow-plugin'
+    ]
 )
